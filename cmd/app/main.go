e11cf491f4ce46ec64dcf77162b7e30016963ff5
Mateo Ferrari Coronel <31248838+mateoferrari97@users.noreply.github.com>
2020-09-09 03:50:45 -0300

    Add db instantiation

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/mateoferrari97/auth/cmd/app/internal"
 	"github.com/mateoferrari97/auth/cmd/server"
 )
 
 func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
 	server := server.NewServer()
 	handler := internal.NewHandler(server)
 	service := internal.NewService()
+
+	dbSettings := fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s",
+		os.Getenv("DATABASE_USER"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_NAME"),
+	)
+
+	db, err := sqlx.Connect("mysql", dbSettings)
+	if err != nil {
+		return fmt.Errorf("instantiating db: %v", err)
+	}
+
+	log.Printf("DB SUCCESS: %s", db.DriverName())
 
 	handler.Ping()
 	handler.RouteMe(service.Authorize)
@@ -17,5 +42,5 @@ func main() {
 	handler.RouteLoginWithGoogleCallback(service.LoginWithGoogleCallback)
 	handler.RouteLogout()
 
-	server.Run(":8081")
+	return server.Run(":8081")
 }
